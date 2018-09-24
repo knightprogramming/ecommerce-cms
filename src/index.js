718cfb54322abd92d0fd39d06aa4a707c2ee2c7d
Taylor <15224340+ttstauss@users.noreply.github.com>
2018-09-24 14:59:35 -0600

    import initial styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import configureStore, { history } from './store/configureStore'
 import Root from './components/Root'
+import './styles/styles.scss'
 
 require('./favicon.ico')
 
