4418dbaa4822c1f36521bf00a49c6e1e76d7a738
Taylor <15224340+ttstauss@users.noreply.github.com>
2018-09-24 17:32:31 -0600

    set up fake login logic
    Lay groundwork for authentication.
    
     - create appWrapper
     - create loadingWrapper
     - create render logic to show Loading component while app loads
     - set up temporary fake login logic (will be modified with legit auth)
     - add erroneously deleted hot module loading setup from react-slingshot

--- src/index.js
+++ src/index.js
@@ -4,14 +4,73 @@ import { AppContainer } from 'react-hot-loader'
 import configureStore, { history } from './store/configureStore'
 import Root from './components/Root'
 import './styles/styles.scss'
+import { logout } from './actions/auth'
+import Loading from './components/base/Loading';
 
 require('./favicon.ico')
 
 const store = configureStore()
 
-render(
+const appWrapper = (
   <AppContainer>
     <Root store={store} history={history} />
-  </AppContainer>,
-  document.getElementById('app')
+  </AppContainer>
 )
+
+const loadingWrapper = (
+  <Loading />
+)
+
+let hasRendered = false
+
+const renderApp = () => {
+  if (!hasRendered) {
+    render(appWrapper, document.getElementById("app"));
+    hasRendered = true;
+  }
+};
+
+render(loadingWrapper, document.getElementById("app"));
+
+/* START temporary 'fake' login logic */
+function select(state) {
+  return state.auth.uid;
+}
+
+const unsubscribe = store.subscribe(checkUser);
+
+function checkUser() {
+  let user = select(store.getState());
+
+  if (user) {
+    unsubscribe();
+    renderApp();
+
+    if (history.location.pathname === "/login") {
+      const location = {
+        pathname: "/login",
+        state: undefined
+      };
+      history.push(location);
+    }
+  } else {
+    unsubscribe();
+    store.dispatch(logout());
+    renderApp();
+  }
+}
+
+checkUser();
+/* END temporary 'fake' login logic */
+
+if (module.hot) {
+  module.hot.accept("./components/Root", () => {
+    const NewRoot = require("./components/Root").default;
+    render(
+      <AppContainer>
+        <NewRoot store={store} history={history} />
+      </AppContainer>,
+      document.getElementById("app")
+    );
+  });
+}
